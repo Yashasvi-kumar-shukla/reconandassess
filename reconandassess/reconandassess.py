bfb04eb5b4be3e5ae2b9111dca2300428f8b5b0c
alpha rinzler <yashasvishukla2002@gmail.com>
2025-02-10 14:24:05 +0530

    Update reconandassess.py

--- reconandassess/reconandassess.py
+++ reconandassess/reconandassess.py
@@ -101,7 +101,7 @@ def run_subzy(target):
     
 def run_nmap(target):
     print(f"[+] Running Nmap scan on {target}...")
-    nmap_cmd = f"nmap -T3 -A -v {target} -oN nmap_scan_results[{target}].txt"
+    nmap_cmd = f"nmap -T3 -A -v --script vuln {target} -oN nmap_scan_results[{target}].txt"
     subprocess.run(nmap_cmd, shell=True)
     output_file = f"nmap_scan_results[{target}].txt"
     full_path = os.path.abspath(output_file)
