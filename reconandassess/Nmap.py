930ed1d001dfc7d927640cb089b27334d5d7a4ac
alpha rinzler <yashasvishukla2002@gmail.com>
2025-02-14 12:40:27 +0530

    Update Nmap.py

--- reconandassess/Nmap.py
+++ reconandassess/Nmap.py
@@ -2,10 +2,16 @@ import subprocess
 import os
 
 class nmap_scan:
-	def run_nmap(target):
-	    print(f"[+] Running Nmap scan on {target}...")
-	    nmap_cmd = f"nmap -T3 -A -v {target} -oN nmap_scan_results[{target}].txt"
-	    subprocess.run(nmap_cmd, shell=True)
-	    output_file = f"nmap_scan_results[{target}].txt"
-	    full_path = os.path.abspath(output_file)
-	    print(f"[+] Nmap scan completed. Results saved at: {full_path}")
+        def get_target_folder(target):
+            folder = f"results/{target}"
+            os.makedirs(folder, exist_ok=True)  # Ensure the directory exists
+            return folder
+            
+        def run_nmap(target):
+            print(f"[+] Running Nmap scan on {target}...")
+            folder = nmap_scan.get_target_folder(target)
+            output_file = f"{folder}/nmap_scan_results.txt"
+            nmap_cmd = f"nmap -T3 -A -v {target} -oN {output_file}"
+            subprocess.run(nmap_cmd, shell=True)
+            full_path = os.path.abspath(output_file)
+            print(f"[+] Nmap scan completed. Results saved at: {full_path}")
